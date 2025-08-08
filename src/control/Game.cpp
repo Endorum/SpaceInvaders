f80f4bbfb8cee658d42472a6cfd4fb6ff638b85a
Anton Pfeifer <mpfeife3@uni-muenster.de>
2025-08-08 16:11:46 +0200

    feat: aliens are displayed in 4 rows of 10

--- src/control/Game.cpp
+++ src/control/Game.cpp
@@ -15,6 +15,20 @@ Game::Game() : window(sf::VideoMode({constants::VIEW_WIDTH, constants::VIEW_HEIG
     game_layer.set_view(view);
 }
 
+// 5 rows x 10 elms 
+void Game::place_aliens(int amount, sf::Texture& texture, int rows, int elms){
+    float x_pos = 0;
+    float y_pos = 0;
+
+    aliens.clear();
+    for(int i = 0; i < amount; i++){
+        x_pos = constants::ALIEN_START_X + (i % elms) * constants::ALIEN_SPACING_X; // 1.5f is the space between aliens
+        y_pos = constants::ALIEN_START_Y + (i / elms) * constants::ALIEN_SPACING_Y; // 1.5f is the space between aliens
+        Alien alien(x_pos, y_pos, texture);
+        aliens.push_back(alien);
+    }
+}
+
 void Game::start() {
     // The clock is needed to control the speed of movement
     sf::Clock clock;
@@ -37,15 +51,8 @@ void Game::start() {
         exit(1);
     }
 
-    // create aliens
-    // place_aliens(50, &alien_texture);
-
-    alien = new Alien(0,0,alien_texture);
-
-
-
-
-    
+    // place aliens in the game
+    place_aliens(40, alien_texture);
 
     while (window.isOpen()) {
         // Restart the clock and save the elapsed time into elapsed_time
@@ -100,6 +107,12 @@ void Game::update(float time_passed) {
     //processInput();
 }
 
+void Game::add_aliens_to_layer() {
+        for(int i=0;i<aliens.size();i++){
+            game_layer.add_to_layer(aliens.at(i).get_sprite());
+        }
+}
+
 void Game::draw() {
     window.clear();
 
@@ -109,10 +122,8 @@ void Game::draw() {
     // add player sprite to layer
     game_layer.add_to_layer(player->get_sprite());
 
-    game_layer.add_to_layer(alien->get_sprite());
-
     // add alien sprites to layer
-    // add_aliens_to_layer();
+    add_aliens_to_layer();
     
 
 
--- src/control/Game.hpp
+++ src/control/Game.hpp
@@ -22,39 +22,12 @@ public:
     // start the game
     void start();
 
-    // 5 rows x 10 elms 
-    void place_aliens(int amount, sf::Texture* texture, int rows=5, int elms=10){
 
-        // int x_pos = 0;
-        // int y_pos = 0;
-
-        // int gap = 1; // in px
-
-        // for(int i=0;i<amount;i++){
-
-        //     x_pos += 1;
-            
-        //     if(i % elms == 0){
-        //         x_pos = 0;
-        //         y_pos += 1;
-        //     }
-
-
-        //     Alien alien(x_pos,y_pos,texture);
-
-        //     aliens.push_back(alien);
-        // }
-    }
-
-    void add_aliens_to_layer(){
-        // for(int i=0;i<aliens.size();i++){
-        //     game_layer.add_to_layer(aliens.at(i).get_sprite());
-        // }
-    }
 
 private:
     // processes user input, returns true if window has been closed
     bool input();
+    void place_aliens(int amount, sf::Texture& texture, int rows = 5, int elms = 10);
     void processInput();
 
     // updates all game elements
@@ -65,14 +38,15 @@ private:
 
     sf::RenderWindow window;
 
+    void add_aliens_to_layer();
+
     // view area and layers
     sf::View view;
     Layer game_layer;
 
-    
     Player* player;
-    
-    Alien* alien;
+
+    std::vector<Alien> aliens;
 
 };
 
