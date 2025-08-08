a13baa240ed0e35fa9180313d90a192e71e00d2d
antonpfeifer <58185425+antonpfeifer@users.noreply.github.com>
2025-08-08 19:05:33 +0200

    Revert "feat: aliens bouncing back and forth and moving downwards"

--- src/model/Constants.hpp
+++ src/model/Constants.hpp
@@ -2,15 +2,13 @@
 
 // define some constants to make code more readable
 namespace constants {
-    constexpr float VIEW_WIDTH = 800.0f;
-    constexpr float VIEW_HEIGHT = 800.0f;
+    constexpr int VIEW_WIDTH = 600;
+    constexpr int VIEW_HEIGHT = 600;
     constexpr unsigned int FRAME_RATE = 60;
     constexpr int ALIEN_ROWS = 5;
     constexpr int ALIEN_COLUMNS = 10;
     constexpr float ALIEN_START_X = 0.0f; // starting x position for the first alien
-    constexpr float ALIEN_START_Y = -600.0f; // starting y position for the first alien
+    constexpr float ALIEN_START_Y = -500.0f; // starting y position for the first alien
     constexpr float ALIEN_SPACING_X = 50.0f;
     constexpr float ALIEN_SPACING_Y = 50.0f;
-    constexpr float ALIEN_SPEED = 100.0f; // speed of alien movement in pixels per second
-    constexpr float ALIEN_SPEED_VERTICAL = 200.0f; 
 }
